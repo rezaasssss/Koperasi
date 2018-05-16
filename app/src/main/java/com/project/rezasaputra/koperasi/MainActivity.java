f0622aca0f7030967733ce9be12a62e49226ee11
reza <rezasaputra.muhammad@yahoo.com>
2018-05-16 15:26:29 +0700

    Initial commit

---
+++ app/src/main/java/com/project/rezasaputra/koperasi/MainActivity.java
@@ -0,0 +1,29 @@
+package com.project.rezasaputra.koperasi;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        },SPLASH_SCREEN);
+
+    }
+}
